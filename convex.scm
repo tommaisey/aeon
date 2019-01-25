b5a0f2606d9f6bb19e160587339a1faafd57b30f
Tom Maisey <>
2019-01-25 09:16:12 +0000

    Add sine wave tests

--- convex.scm
+++ convex.scm
@@ -8,9 +8,9 @@
 			  ".sls"
 			  ".sch"))
 (library-directories (list "."
-			   "~/Code/scheme-libs"
-			   "~/Code/scheme-libs/thunderchez"
-			   "~/Code/scheme-libs/rsc3"))
+			   "~/Code/convex-deps"
+			   "~/Code/convex-deps/thunderchez"
+			   "~/Code/convex-deps/rsc3"))
 (optimize-level 2)
 
 (import (rnrs)
@@ -21,5 +21,21 @@
 	(pattern-node)
 	(context)
 	(rsc3)
-	(sosc))
+	(sosc)
+	(rhs)) ;; rhs for use in rsc3 examples
+
+;; TODO: check that SuperCollider is running. I think I'll need
+;; to implement select(2) in (sockets ffi) and (sockets syntax),
+;; then expose that from the sosc/ext/ip.chez.scm udp interface.
+
+;; Some SuperCollider test sounds...
+(define (sine-perc freq time)
+  (let* ([d (env-perc 0.01 time 1 (list -4 -4))]
+	 [e (env-gen kr 1 0.1 0 1 remove-synth d)])
+    (audition (out 0 (mul e (sin-osc ar freq 0))))))
+
+(define (sine-smooth freq time)
+  (let* ([d (env-sine time 0.1)]
+	 [e (env-gen kr 1 0.2 0 1 remove-synth d)])
+  (audition (out 0 (mul e (sin-osc ar freq 0))))))
 
