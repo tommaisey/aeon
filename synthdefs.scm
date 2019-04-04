ce3b7b214f54b27afa7ff4e18010cea8256743a7
Tom Maisey <tom.maisey@roli.com>
2019-04-04 20:05:51 +0100

    Tweak synthdefs

--- synthdefs.scm
+++ synthdefs.scm
@@ -57,8 +57,8 @@
 (define (make-rand-lfo mag time)
   (mul-add (lfd-noise1 ar time) 1 mag))
 
-;; N-arity versions of rsc3 mul, sub & add. Can DRY these once I understand
-;; macro composition better...
+;; N-arity versions of rsc3 mul, sub & add. Can DRY these once I
+;; understand macro composition better...
 (define-syntax mul-n
   (syntax-rules ()
     ((_ x) x)
@@ -91,74 +91,73 @@
 
 ;;-------------------------------------------------------------------
 ;; The synthdef nursery - quite bare at the moment!
-(send-synth
- sc3 "sine-grain"
- (letc ([:freq 440] [:attack 0.01] [:sustain 1] [:amp 0.2] [:pan 0.5])
-   
-   (let* ([osc (sin-osc ar :freq 0)]
-	  [env (env-perc :attack :sustain 1 (repeat -4 2))]
-	  [env (make-env-gen :amp env remove-synth)])
-     (out 0 (pan2 (mul env osc)
-		  (mul-add :pan 2 -1) 1)))))
+(send-synth sc3 "sine-grain"
+  (letc ([:freq 440] [:attack 0.01] [:sustain 1]
+	 [:amp 0.5] [:pan 0.5])
+    
+    (let* ([osc (sin-osc ar :freq 0)]
+	   [env (env-perc :attack :sustain 1 (repeat -4 2))]
+	   [env (make-env-gen :amp env remove-synth)])
+      (out 0 (pan2 (mul env osc)
+		   (mul-add :pan 2 -1) 1)))))
 
-(send-synth
- sc3 "sampler"
- (letc ([:sample 0] [:attack 0.01] [:sustain 1] [:pan 0.5]
-	[:speed 1] [:release 0.25] [:amp 0.3])
-   
-   (let* ([rte (mul :speed (buf-rate-scale kr :sample))]
-	  [osc (play-buf 1 ar :sample rte 1 0 no-loop remove-synth)]
-	  [env (make-linen :amp :attack :sustain :release -4)])
-     (out 0 (pan2 (mul env osc)
-		  (mul-add :pan 2 -1) 1)))))
-(send-synth
- sc3 "swirly-keys"
- (letc ([:freq 440] [:attack 0.4] [:sustain 1] [:release 1]
-	[:amp 0.2] [:pan 0.5] [:resonance 0.1] [:cutoff 3] [:verb-amt 0.1])
-   
-   (let* ([osc1 (lf-saw ar (add :freq -0.5) 0)]
-	  [osc2 (lf-saw ar (add :freq +1.5) 0.3)]
-	  [amp-env (make-linen :amp :attack :sustain :release -4)]
-	  [cut-env (make-linen :cutoff :attack :sustain :release 2 do-nothing)]
-	  [flt (moog-ff (add osc1 osc2) (add :freq (mul :freq cut-env)) :resonance 0.0)])
-     (out 0 (pan2 (mul amp-env flt) 
-		  (mul-add :pan 2 -1) 1)))))
+(send-synth sc3 "sampler"
+  (letc ([:attack 0.01] [:sustain 1] [:release 0.25]
+	 [:sample 0] [:speed 1]
+	 [:amp 0.3] [:pan 0.5])
+    
+    (let* ([rate (mul :speed (buf-rate-scale kr :sample))]
+	   [osc  (play-buf 1 ar :sample rate 1 0 no-loop remove-synth)]
+	   [env  (make-linen :amp :attack :sustain :release -4)])
+      (out 0 (pan2 (mul env osc)
+		   (mul-add :pan 2 -1) 1)))))
 
-(send-synth
- sc3 "phase-tris"
- (letc ([:freq 440] [:attack 0.4] [:sustain 1] [:release 1]
-	[:amp 0.2] [:pan 0.5] [:resonance 0.1])
+(send-synth sc3 "swirly-keys"
+  (letc ([:attack 0.4] [:sustain 1] [:release 1]
+	 [:freq 440] [:amp 0.2] [:pan 0.5]
+	 [:resonance 0.1] [:cutoff 3] [:verb-amt 0.1])
+    
+    (let* ([osc1 (lf-saw ar (mul :freq 0.9946) 0)]
+	   [osc2 (lf-saw ar (mul :freq 1.0024) 0.3)]
+	   [amp-env (make-linen :amp :attack :sustain :release -4)]
+	   [cut-env (make-linen :cutoff :attack :sustain :release 2 do-nothing)]
+	   [cutoff (add :freq (mul :freq cut-env))]
+	   [flt (moog-ff (add osc1 osc2) cutoff :resonance 0.0)])
+      (out 0 (pan2 (mul amp-env flt) 
+		   (mul-add :pan 2 -1) 1)))))
 
-   (define (make-osc mag freq-mul time)
-     (lf-tri ar (make-rand-lfo (mul-n :freq freq-mul mag) time) 0))
-   
-   (let* ([osc1 (make-osc 0.010 0.5 1/2)]
-	  [osc2 (make-osc 0.008 1.0 1)]
-	  [osc3 (make-osc 0.002 1.5 2)]
-	  [env (make-linen :amp :attack :sustain :release -4)])
-     (out 0 (pan2 (mul env (add (add osc1 osc2) osc3))
-		  (mul-add :pan 2 -1) 1)))))
+(send-synth sc3 "phase-tris"
+  (letc ([:attack 0.4] [:sustain 1] [:release 1]
+	 [:freq 440] [:amp 0.2] [:pan 0.5]
+	 [:resonance 0.1])
 
-(send-synth
- sc3 "dual-lopass"
- (letc ([:freq 440] [:attack 2.0] [:sustain 1] [:release 1]
-	[:amp 0.2] [:pan 0.5] [:resonance 0.1] [:width 0.2])
+    (define (make-osc mag freq-mul time)
+      (lf-tri ar (make-rand-lfo (mul-n :freq freq-mul mag) time) 0))
+    
+    (let* ([osc1 (make-osc 0.010 0.5 1/2)]
+	   [osc2 (make-osc 0.008 1.0 1)]
+	   [osc3 (make-osc 0.002 1.5 2)]
+	   [env (make-linen :amp :attack :sustain :release -4)])
+      (out 0 (pan2 (mul env (add (add osc1 osc2) osc3))
+		   (mul-add :pan 2 -1) 1)))))
 
-   (define (make-osc freq flt-atk width-mul)
-     (let* ([osc (lf-pulse ar freq 0 0.5)]
-	    [osc-flt-env (make-linen 1 flt-atk :sustain :release 1 do-nothing)]
-	    [osc-flt-env (add-n freq
-				(mul freq width-mul)
-				(mul-n 4 osc-flt-env freq))])
-       (moog-ff osc osc-flt-env :resonance 0)))
+(send-synth sc3 "dual-lopass"
+  (letc ([:attack 2.0] [:sustain 1] [:release 1]
+	 [:freq 440] [:amp 0.2] [:pan 0.5]
+	 [:resonance 0.1] [:width 0.2])
+    
+    (define (make-osc freq flt-atk width-mul)
+      (let* ([osc (lf-pulse ar freq 0 0.5)]
+	     [osc-flt-env (make-linen 1 flt-atk :sustain :release 1 do-nothing)]
+	     [osc-flt-env (add-n freq
+				 (mul freq width-mul)
+				 (mul-n 4 osc-flt-env freq))])
+	(moog-ff osc osc-flt-env :resonance 0)))
 
-   (let* ([osc1 (make-osc (mul :freq 0.5) :attack 0)]
-	  [osc2 (make-osc :freq (mul :attack 1.5) :width)]
-	  [amp (mul :amp (lin-lin :freq 20 10000 1.0 0.2))]
-	  [amp-env (make-linen amp :attack :sustain :release -4)])
-     (out 0 (pan2 (mul (add osc1 osc2) amp-env)
-		  (mul-add :pan 2 -1)
-		  1)))))
-
-;; Extra overriden functions from rsc3 - I prefer to leave that source as it
-;; is for now.
+    (let* ([osc1 (make-osc (mul :freq 0.5) :attack 0)]
+	   [osc2 (make-osc :freq (mul :attack 1.5) :width)]
+	   [amp (mul :amp (lin-lin :freq 20 10000 1.0 0.2))]
+	   [amp-env (make-linen amp :attack :sustain :release -4)])
+      (out 0 (pan2 (mul (add osc1 osc2) amp-env)
+		   (mul-add :pan 2 -1)
+		   1)))))
