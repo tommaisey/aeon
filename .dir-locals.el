bddf22ceca3619bbd7cc24d3427cb539331c4214
Tom Maisey <tommaisey@gmail.com>
2021-03-13 16:28:11 +0000

    Add .dir-locals.el emacs project config

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((nil . ((indent-tabs-mode . nil)))
+ (scheme-mode . ((geiser-active-implementations . (chez . nil)))))
