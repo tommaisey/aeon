9daeddf52e2758201781b6487878eefe506d20f2
Tom Maisey <tom.maisey@roli.com>
2021-04-14 23:46:25 +0100

    Add (bits) and (atoi)

--- libs/rhythm.scm
+++ libs/rhythm.scm
@@ -3,9 +3,23 @@
 ;; Rhythm helper functions
 ;; ---------------------------------------------------------
 (library (rhythm)
-  (export rhythm-docs euc)
+  (export rhythm-docs bits atoi euc)
 
   (import (chezscheme) (utilities) (doc))
+
+  ;; Returns a list of length num-bits, of binary bits contained in 'n'.
+  ;; In other words, it's a fun way to generate sequences from numbers.
+  (define* (bits num-bits n [/opt (off-val '~) (on-val 1)])
+    (define (val i) (if (bitwise-bit-set? n i) on-val off-val))
+    (map val (atoi (dec num-bits))))
+
+  ;; Nope, not the integer-parsing function from C!
+  ;; The opposite/reverse of Scheme's iota function.
+  ;; Returns a list of integers from 'n' to 0.
+  (define (atoi n)
+    (if (< n 0)
+        (list)
+        (cons n (atoi (dec n)))))
 
   ;;-----------------------------------------------------------------
   ;; Euclidean patterns
@@ -21,13 +35,13 @@
            [bucket (+ bucket (* num-hits (mod step num-steps)))])
       (not (eqv? (quotient bucket num-steps)
                  (quotient (+ bucket num-hits) num-steps)))))
-  
+
   (define* (euc num-steps num-hits [/opt (offset 0) (on-val 1) (off-val '~)])
     (map (lambda (i) (if (euc-hit? i num-steps num-hits offset) on-val off-val))
          (iota num-steps)))
 
   (make-doc rhythm-docs
-    (euc 
+    (euc
      "Returns a list representing a euclidean pattern."
      ((steps Number "The number of steps in the rhythm")
       (hits  Number "The number of active steps in the rhythm")
@@ -39,6 +53,6 @@
       ((euc 8 3) => (1 ~ ~ 1 ~ ~ 1 ~))
       ((euc 3 1 1) => (~ 1 ~))
       ((euc 3 1 1 "hi" 0) => (0 "hi" 0)))))
-  
-  
+
+
   ); end module 'rhythm'
